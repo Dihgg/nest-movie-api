fddf465a449cb67a15b6f7eb64a4e752ca9515fb
Diego Lopes <dihgg@dihgg.com>
2021-05-25 11:41:59 -0300

    user swagger documentation

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import {Controller, Get, Post, Body, Param, Query} from '@nestjs/common';
 import { UsersService } from './users.service';
 import {CreateUserDto} from "./dto/create-user.dto";
+import {ApiResponse, ApiTags} from "@nestjs/swagger";
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiTags('users')
+  @ApiResponse({ status: 201, description: 'The user has been successfully created.'})
+  @ApiResponse({ status: 422, description: 'The user already exists.'})
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);

fcfbdd0a31b30aea59acb372976de217ed24de34
Diego Lopes <dihgg@dihgg.com>
2021-05-25 12:07:33 -0300

    swagger for auth

--- api/src/main.ts
+++ api/src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
       .setDescription('CHALLENGE INC Movies App')
       .setVersion('1.0')
       .addTag('movies')
+      .addBasicAuth()
+      .addBearerAuth()
       .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
--- api/src/users/dto/login-user.dto.ts
+++ api/src/users/dto/login-user.dto.ts
@@ -1,7 +1,19 @@
 import {IUser} from "../entities/user.interface";
+import {ApiProperty} from "@nestjs/swagger";
 
 export class LoginUserDto implements IUser {
+    @ApiProperty()
     id: number;
+
+    @ApiProperty()
     username: string;
+
+    @ApiProperty()
     password: string;
+}
+
+export class LoginDto {
+
+    @ApiProperty()
+    user: LoginUserDto;
 }
